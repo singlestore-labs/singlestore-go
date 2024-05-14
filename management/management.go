8c7929631d1aa38704d1059be88687126f36b97d
CI <ci@singlestorelabs.com>
2024-05-14 01:55:06 +0000

    Updating the client to version management/v1.1.66

--- management/management.go
+++ management/management.go
@@ -2716,7 +2716,7 @@ func NewGetV1StageWorkspaceGroupIDFsRequest(server string, workspaceGroupID open
 		return nil, err
 	}
 
-	operationPath := fmt.Sprintf("/v1/stage/%s/fs/", pathParam0)
+	operationPath := fmt.Sprintf("/v1/stage/%s/fs", pathParam0)
 	if operationPath[0] == '/' {
 		operationPath = "." + operationPath
 	}
